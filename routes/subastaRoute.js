016bf49f8c161929a688038ed2a60dba5e45e25c
jujosa09 <jujosadols@uma.es>
2023-10-21 13:06:08 +0200

    Revert "feat: añadido modelo de valoracion, crear valoracion y borrar valoracion, tambien modificado crud de subasta para borrar subasta"
    This reverts commit a7f767a91b8acc907705b9f25607b22fe3f85366.

--- routes/subastaRoute.js
+++ routes/subastaRoute.js
@@ -1,15 +1,14 @@
 const express = require('express')
 
-const { createSubastaController, getSubastaByIdController } = require('../controllers/subastaController')
+const { createSubastaController, getSubastaByIdController, deleteSubastaController, actualizarSubastaController } = require('../controllers/subastaController')
 
 const router = express.Router()
 
 router.post('/subasta', createSubastaController)
-router.get('/subasta/:id', getSubastaByIdController)
-
+router.get('/subasta', getSubastaByIdController)
+router.delete('/subasta/:id',  deleteSubastaController)
+router.put('/subasta', actualizarSubastaController)
 
 module.exports = {
     router
 }
-
-
